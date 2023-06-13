9a2f216f70ac8b5bc19cbeaa1ea46f45d6c7068c
serena_aneli <serena.aneli@gmail.com>
2023-06-13 15:40:10 +0200

    Update and rename primer_specificity_blast_code.py to primer_specificity_blast.py

--- multiplex_blast_search/primer_specificity_blast.py
+++ multiplex_blast_search/primer_specificity_blast.py
@@ -160,23 +160,3 @@ def find_amplicons(primers_file, output_file):
 							len(j_seq), row_j['aln_identities']/len(j_seq)*100, row_j['aln_gaps'], row_j['aln_query_start'], row_j['aln_query_end'], notes,
 							sep="\t", file = found_alignments
 							)
-
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Primer blast search and amplicon size check')
-	parser.add_argument(
-		'primer_list_path', metavar='primers', type=str, 
-		help='path to the primer list files'
-		)
-
-	args = parser.parse_args()
-
-
-	tab = read_primer_file(args.primer_list_path)
-
-	for index, row in tab.iterrows():
-		print('searching {marker}, {pair} in BLAST'.format(marker=row['marker'], pair=row['type']))
-		blast_search('blast_results', row['marker'], row['type'], row['seq']) # NON MI STAMPA IL MESSAGGIO CHE HA TROVATO
-		search = row['marker'] + '_' + str(row['type'])
-		alignment_parsing(search + '.xml')
-
-	find_amplicons('primers.txt', 'output_prova.tsv')
